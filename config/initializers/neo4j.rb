5bcf3ac952b27808690993ffacf73a6934104682
Veselin Todorov <veselin@veselin.bg>
2014-10-26 17:02:20 +0200

    Add auth for neo4j

--- config/initializers/neo4j.rb
+++ config/initializers/neo4j.rb
@@ -5,4 +5,6 @@ port = test ? ENV['NEO4J_TEST_PORT'] : ENV['NEO4J_PORT']
 Neography.configure do |config|
   config.server = host
   config.port   = port
+  config.username = ENV['NEO4J_USER']
+  config.password = ENV['NEO4J_PASS']
 end
